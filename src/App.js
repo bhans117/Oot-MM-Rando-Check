77c87d11a58c46578bb8abf8dbca4618ac244c41
Benjamin Hansen <brhansen3@wisc.edu>
2023-03-11 18:34:57 -0600

    add GA

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { SWRConfig } from 'swr'
 import Dashboard from './components/Dashboard';
 import { localStorageFetcher } from './utils/fetchers'
 import { ProviderSpoilerLog } from './contextSpoilerLog'
+import ReactGA from 'react-ga'
+import { useEffect } from 'react';
+
+ReactGA.initialize("G-9VVT8FC5P9")
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, []);
+
   return (
     <SWRConfig
       value={{
